521531706f179e4efab38349466c50595348ec40
Brian Johnson <brianjohnsona@yahoo.com>
2019-09-22 18:55:02 -0500

    added constructor method

--- src/App.js
+++ src/App.js
@@ -5,14 +5,25 @@ import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <List />
-      <Note />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
+  render
+    const { showNote } = this.state;
+
+    return (
+      <div className="App">
+      
+        <Nav />
+        { showNote ? <Note /> : <List /> }
+      </div>
+    );
+  }
 }
 
 export default App;
