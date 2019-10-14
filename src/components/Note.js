394c4402babcbce4aaa9fca16f0f2234cb2c0da4
Brian Johnson <brianjohnsona@yahoo.com>
2019-10-14 18:15:01 -0500

    fixed code

--- src/components/Note.js
+++ src/components/Note.js
@@ -11,13 +11,20 @@ class Note extends React.Component {
     	this.props.submitNote(formData, this.props.note.id);
   }
 
-  renderTagForm() {
+  class="language-jsx">renderTagForm() {
+   if (!this.props.newTag) {
   	return (
     <span>
       Tag your note:
-      <i className="tag-button material-icons">add circle</i>
+      <i
+          className="tag-button material-icons"
+          onClick={() => this.props.showTagForm()}
+        >
+            add circle
+        </i>
     </span>
-  );
+    );
+   }
   }
 
 	render() {
